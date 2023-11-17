75229517770535853e1db86db3f890d28e134424
milaura22@gmail.com <milaura22@gmail.com>
2023-11-17 08:24:50 +0800

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+  resources :lists, only: [:index, :create, :new, :show, :destroy] do
+    resources :bookmarks, only: [:create, :new]
   end
   resources :bookmarks, only: [:destroy]
 end
